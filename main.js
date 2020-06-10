75aab90bc3860cec90b9986df946be76e737e607
ShedrackGodson <hedrickgodson23@gmail.com>
2020-06-10 16:57:16 +0300

    Arrows functions and Interfaces

--- main.js
+++ main.js
@@ -1,6 +1,32 @@
-var variable = 10;
-variable = "Shedrack";
+// // let can not accept variable re-assignment 
+// // let number = 10;
+// // number = 'Ten';
+// // Also the scope of variable when using let keyword 
+// function doSomething(name) {
+//     for(let i=0; i<5; i++) {
+//         let x = i;
+//         x *= 2;
+//         console.log(x.toString()+" "+name);
+//     }
+// }
+// // doSomething("Shedrack Godson");
+// enum Color { Red, Blue, Green }
+// let backgroundColor = Color.Blue;
+var message;
+message = 'hello';
+var endsWithO = message.endsWith('o');
+var alternatively = message.endsWith('o');
+// Arrow functions
+var Name;
+(function (Name) {
+    Name[Name["Firstname"] = 0] = "Firstname";
+    Name[Name["Middlename"] = 1] = "Middlename";
+    Name[Name["Surname"] = 2] = "Surname";
+})(Name || (Name = {}));
+var firstname = Name.Firstname;
+var secondname = Name.Middlename;
+var lastname = Name.Surname;
 
-
-console.log(typeof(variable));
-console.log(variable);
+console.log(firstname);
+console.log(secondname);
+console.log(lastname);
--- main.ts
+++ main.ts
@@ -1,4 +1,38 @@
-let variable = 10;
+// Arrows functions
+let log = function (message) {
+  console.log(message);
+}
+
+// log("Hello t/his my first tutorial about TypeScript i.e A Javascript subset");
+
+// console.log("There is much simpler way to dclare functions in TypeScript");
+
+let doLog = (name, message) => console.log("My name is "+name," and this is my message"+message+"\n");
+
+// doLog("Shedrack Godson Lucas",": This is an arrow function");
+
+// Inline Annotartions
+
+let drawPoint = (point: { name: string, year: number }) => {
+  // code...
+}
+
+drawPoint({
+  name: "Benz",
+  year: 2002
+})
 
 
-variable = "Shedrack";
+// Concepts of Interfaces
+/* When using Interfaces always use Naming Convertions i.e
+  Starts with the capital letter.
+*/
+interface Point{
+  name: String,
+  year: Number
+}
+
+let drawingPoint = (point: Point) => {
+  // Code...
+}
+
